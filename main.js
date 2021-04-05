601f4a16ea71254033e91943d659536230e97705
David Matalik <david.matalik@posteo.de>
2021-04-05 09:29:51 +0200

    Initialize project with webpack and jest

---
+++ main.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+function test() {
+  console.log('Everything still works')
+}
+
+test()
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
