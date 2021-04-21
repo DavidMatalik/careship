1fc42008084957dda7e481c6614460b2559ee8cc
David Matalik <david.matalik@posteo.de>
2021-04-21 08:09:36 +0200

    Add basic styling to App

--- src/gameView.js
+++ src/gameView.js
@@ -1,19 +1,58 @@
+import PubSub from 'pubsub-js'
+
 export default (function () {
+  const body = document.querySelector('body')
+
+  const heading = document.createElement('h1')
+  const boardsContainer = document.createElement('div')
+  const board1Container = document.createElement('div')
+  const board2Container = document.createElement('div')
+  const playerName = document.createElement('h2')
+  const computerName = document.createElement('h2')
+  const board1 = document.createElement('div')
+  const board2 = document.createElement('div')
+  const form = document.createElement('form')
+  const nameLabel = document.createElement('label')
+  const nameInput = document.createElement('input')
+  const startButton = document.createElement('button')
+  const finishMessage = document.createElement('p')
+
   const createBoardFields = (size, board) => {
-    for (let i = 0; i < size * size; i++) {
-      const field = document.createElement('div')
-      field.classList.add('field')
-      board.appendChild(field)
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        const field = document.createElement('div')
+        field.classList.add('field')
+        field.dataset.coords = `${i}${j}`
+        board.appendChild(field)
+      }
     }
   }
 
-  const createBoards = (boardSize) => {
-    const boardsContainer = document.createElement('div')
-    const board1 = document.createElement('div')
-    const board2 = document.createElement('div')
+  const addFieldListeners = (board) => {
+    const fields = Array.from(board.children)
+    fields.forEach((field) => {
+      field.addEventListener('click', publishCoords)
+    })
+  }
 
-    boardsContainer.appendChild(board1)
-    boardsContainer.appendChild(board2)
+  const publishCoords = (event) => {
+    const coords = event.target.dataset.coords
+    const coordsArray = [parseInt(coords[0]), parseInt(coords[1])]
+    PubSub.publish('fieldClicked', coordsArray)
+  }
+
+  const createBoards = (boardSize) => {
+    board1Container.appendChild(playerName)
+    board2Container.appendChild(computerName)
+
+    board1Container.appendChild(board1)
+    board2Container.appendChild(board2)
+
+    boardsContainer.appendChild(board1Container)
+    boardsContainer.appendChild(board2Container)
+
+    playerName.classList.add('name')
+    computerName.classList.add('name')
 
     boardsContainer.id = 'board-container'
     board1.id = 'board-1'
@@ -25,28 +64,109 @@ export default (function () {
     return boardsContainer
   }
 
-  const loadStartpage = (boardSize) => {
-    const body = document.querySelector('body')
-    const heading = document.createElement('h1')
-    const form = document.createElement('form')
-    const nameLabel = document.createElement('label')
-    const nameInput = document.createElement('input')
-    const startButton = document.createElement('button')
+  const resetBoard = (board) => {
+    const fields = Array.from(board.children)
+    fields.forEach((field) => {
+      field.innerHTML = ''
+    })
+  }
 
-    body.appendChild(heading)
-    body.appendChild(createBoards(boardSize))
-    body.appendChild(form)
+  const startGame = (event) => {
+    // If input makes sense the if below is executed
+    if (form.checkValidity()) {
+      // preventDefault() prevents page from relaoding
+      event.preventDefault()
+      PubSub.publish('startClicked', nameInput.value)
+      form.style.display = 'none'
+      playerName.innerHTML = `${nameInput.value}'s board`
+      computerName.innerHTML = `Computer's board`
+      finishMessage.style.display = 'none'
+      resetBoard(board1)
+      resetBoard(board2)
+      addFieldListeners(board2)
+    }
+  }
 
+  const createForm = () => {
     form.appendChild(nameLabel)
     form.appendChild(nameInput)
     form.appendChild(startButton)
 
-    heading.innerHTML = 'Careship'
-
     nameLabel.innerHTML = 'Name: '
     nameInput.placeholder = 'Enter your name'
-    startButton.innerHTML = 'Start Game'
+    nameInput.required = true
+
+    startButton.innerHTML = 'Start new Game'
+
+    form.addEventListener('submit', startGame)
+    return form
   }
 
-  return { loadStartpage }
+  const init = (boardSize) => {
+    body.appendChild(heading)
+    body.appendChild(finishMessage)
+    body.appendChild(createBoards(boardSize))
+    body.appendChild(createForm())
+
+    heading.innerHTML = 'Careship'
+
+    finishMessage.id = 'finish-message'
+    finishMessage.style.display = 'none'
+
+    PubSub.subscribe('changedPlayerboard', updatePlayerboard)
+    PubSub.subscribe('changedComputerboard', updateComputerboard)
+    PubSub.subscribe('gameFinished', renderFinishDisplay)
+  }
+
+  const updatePlayerboard = (msg, boardDetails) => {
+    // Hier kommt ein 2D-Array rein mit undefined/object/true/false
+    // Erstmal mit "o" "t" "f" rendern - später besser rendern
+    for (let i = 0; i < boardDetails.length; i++) {
+      for (let j = 0; j < boardDetails.length; j++) {
+        const field = board1.querySelector(`[data-coords='${i}${j}']`)
+        if (typeof boardDetails[i][j] === 'object') {
+          field.innerHTML = 'ship'
+        }
+        if (boardDetails[i][j] === false) {
+          field.innerHTML = 'no!'
+        }
+        if (boardDetails[i][j] === true) {
+          field.innerHTML = 'yes!'
+        }
+      }
+    }
+  }
+
+  const updateComputerboard = (msg, boardDetails) => {
+    // Hier kommt ein 2D-Array rein mit undefined/object/true/false
+    // Erstmal mit "o" "t" "f" rendern - später besser rendern
+    for (let i = 0; i < boardDetails.length; i++) {
+      for (let j = 0; j < boardDetails.length; j++) {
+        const field = board2.querySelector(`[data-coords='${i}${j}']`)
+        if (boardDetails[i][j] === false) {
+          field.innerHTML = 'no!'
+        }
+        if (boardDetails[i][j] === true) {
+          field.innerHTML = 'yes'
+        }
+      }
+    }
+  }
+
+  const removeFieldListeners = (board) => {
+    const fields = Array.from(board.children)
+    fields.forEach((field) => {
+      field.removeEventListener('click', publishCoords)
+    })
+  }
+
+  const renderFinishDisplay = (msg, name) => {
+    finishMessage.innerHTML = `${name} has supplied all ships!`
+    finishMessage.style.display = 'block'
+    removeFieldListeners(board2)
+    nameInput.value = ''
+    form.style.display = 'block'
+  }
+
+  return { init }
 })()
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-function test() {
-  console.log('Everything still works')
-}
+import gameLogicModule from './gameLogic'
+import gameViewModule from './gameView'
 
-test()
+gameViewModule.init(10)
+gameLogicModule.init()
+
+// Next Steps:
+
+/*
+   So wie es jetzt ist könnte ich mal Commit machen
+   
+   Boards verhauts gerade ziemlich noch wenn ich rauszoome
+
+   Was gerade Problem: auch bei Klick auf bereits vorhandenes Feld macht
+   Computer einen Zug. Wenn auf bereits gecklicktes Feld von Player geklicked wird
+   sollte eig. nichts passieren oder?
+
+   Wie der Code in gameView strukturiert ist gefällt mir teilweise gar nicht:
+   z.B. paar Dom-Element ganz oben erstellt, paar Dom-Elemente in init usw...
+   Vielleicht Alle ganz oben? Und in init nur Funktionen aufrufen?
+   */
