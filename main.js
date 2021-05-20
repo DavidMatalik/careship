bf1be1fbf3ebdfbf980af94fb54a414feebf18a5
David Matalik <david.matalik@posteo.de>
2021-05-20 17:51:11 +0200

    Implement dragging functionality
    For detailed commit steps look here:
    https://github.com/DavidMatalik/drag-and-drop

--- main.js
+++ main.js
@@ -362,6 +362,46 @@
 
 /***/ }),
 
+/***/ "./node_modules/uniqid/index.js":
+/*!**************************************!*\
+  !*** ./node_modules/uniqid/index.js ***!
+  \**************************************/
+/***/ ((module) => {
+
+/* 
+(The MIT License)
+Copyright (c) 2014-2021 Halász Ádám <adam@aimform.com>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+//  Unique Hexatridecimal ID Generator
+// ================================================
+
+//  Dependencies
+// ================================================
+var pid = typeof process !== 'undefined' && process.pid ? process.pid.toString(36) : '' ;
+var address = '';
+if(false){ var i, mac, networkInterfaces; } 
+
+//  Exports
+// ================================================
+module.exports = module.exports.default = function(prefix, suffix){ return (prefix ? prefix : '') + address + pid + now().toString(36) + (suffix ? suffix : ''); }
+module.exports.process = function(prefix, suffix){ return (prefix ? prefix : '') + pid + now().toString(36) + (suffix ? suffix : ''); }
+module.exports.time    = function(prefix, suffix){ return (prefix ? prefix : '') + now().toString(36) + (suffix ? suffix : ''); }
+
+//  Helpers
+// ================================================
+function now(){
+    var time = Date.now();
+    var last = now.last || time;
+    return now.last = time > last ? time : last + 1;
+}
+
+
+/***/ }),
+
 /***/ "./src/gameLogic.js":
 /*!**************************!*\
   !*** ./src/gameLogic.js ***!
@@ -452,6 +492,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var pubsub_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! pubsub-js */ "./node_modules/pubsub-js/src/pubsub.js");
 /* harmony import */ var pubsub_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(pubsub_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var uniqid__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! uniqid */ "./node_modules/uniqid/index.js");
+/* harmony import */ var uniqid__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(uniqid__WEBPACK_IMPORTED_MODULE_1__);
+
 
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((function () {
@@ -473,6 +516,10 @@ __webpack_require__.r(__webpack_exports__);
   const startButton = document.createElement('button')
   const finishMessage = document.createElement('p')
 
+  let draggedShipSections = null
+  let draggedShipCopy = null
+  let shipVerticalPosition = false
+
   const createBoardFields = (size, board) => {
     for (let i = 0; i < size; i++) {
       for (let j = 0; j < size; j++) {
@@ -484,7 +531,94 @@ __webpack_require__.r(__webpack_exports__);
     }
   }
 
-  const addFieldListeners = (board) => {
+  // Check if element would wrap (what is unwanted)
+  const checkWrap = (targetEl) => {
+    const coords = targetEl.dataset.coords
+    const coord = shipVerticalPosition
+      ? parseInt(coords[0])
+      : parseInt(coords[1])
+
+    return parseInt(draggedShipSections) + coord > 10 ? true : false
+  }
+
+  // Check if element would be placed on blocked
+  // field. (what is unwanted)
+  const checkBlocked = (targetEl) => {
+    let blocked = false
+    const fields = getFields(targetEl)
+
+    fields.forEach((field) => {
+      if (field.classList.contains('blocked')) {
+        blocked = true
+      }
+    })
+    return blocked
+  }
+
+  // Returns all fields depending on number of global var sections
+  // and if global var elementVerticalPosition is true or false
+  const getFields = (targetEl) => {
+    let fields = []
+    let coords = parseInt(targetEl.dataset.coords)
+
+    const iterator = shipVerticalPosition ? 10 : 1
+
+    for (let i = draggedShipSections; i > 0; i--) {
+      const field = document.querySelector(
+        `[data-coords='${coords < 10 ? 0 : ''}${coords}']`
+      )
+
+      if (field === null) {
+        break
+      }
+
+      fields.push(field)
+      // Prepare selection of field under current field
+      coords += iterator
+    }
+    return fields
+  }
+
+  // Add color to fields  to highlight them
+  // where mouse is and fields on right to mouse
+  const highlightFields = (ev) => {
+    ev.preventDefault()
+
+    if (checkWrap(ev.target) || checkBlocked(ev.target)) {
+      return
+    }
+
+    const fields = getFields(ev.target)
+    fields.forEach((field) => field.classList.add('highlight'))
+  }
+
+  // For change from elementVerticalPosition to horizontal or otherway
+  function whitenAllFields() {
+    const fields = document.querySelectorAll('.field')
+    fields.forEach((field) => {
+      field.classList.remove('highlight')
+    })
+  }
+
+  // Remove color from previously highlighted
+  // fields when mouse goes somewhere else
+  function whitenFields(ev) {
+    ev.preventDefault()
+
+    const fields = getFields(ev.target)
+    fields.forEach((field) => field.classList.remove('highlight'))
+  }
+
+  const addDragDropListeners = (board) => {
+    const fields = Array.from(board.children)
+    fields.forEach((field) => {
+      field.addEventListener('dragleave', whitenFields)
+      field.addEventListener('dragover', highlightFields)
+      field.addEventListener('drop', placeShip)
+    })
+  }
+
+  const addClickListeners = (board) => {
     const fields = Array.from(board.children)
     fields.forEach((field) => {
       field.addEventListener('click', publishCoords)
@@ -497,46 +631,133 @@ __webpack_require__.r(__webpack_exports__);
     pubsub_js__WEBPACK_IMPORTED_MODULE_0___default().publish('fieldClicked', coordsArray)
   }
 
-  const createDragShips = (name, number) => {
-    for(let i = 0; i < number; i++) {
+  const makeShipDraggable = (ship) => {
+    ship.draggable = 'true'
+    ship.ondragstart = (ev) => {
+      var img = new Image()
+      ev.dataTransfer.setDragImage(img, 0, 0)
+      ev.dataTransfer.setData('text', ev.target.id)
+      ev.dataTransfer.effectAllowed = 'all'
+      draggedShipSections = parseInt(ev.target.dataset.sections)
+
+      draggedShipCopy = ship.cloneNode(true)
+      draggedShipCopy.style.position = 'absolute'
+      draggedShipCopy.style.transformOrigin = 'top left'
+
+      document.body.append(draggedShipCopy)
+
+      document.addEventListener('drag', (ev) => {
+        draggedShipCopy.style.top = ev.pageY + 10 + 'px'
+        draggedShipCopy.style.left = ev.pageX + 'px'
+      })
+
+      document.addEventListener('dragend', () => {
+        draggedShipCopy.remove()
+      })
+    }
+
+    // If ctrl key is pressed and hold
+    // then display draggedElementCopy elementVerticalPosition
+    ship.ondrag = (ev) => {
+      if (ev.ctrlKey) {
+        draggedShipCopy.style.transform = 'rotate(90deg)'
+        if (shipVerticalPosition === false) {
+          shipVerticalPosition = true
+          // Without whitenAllFields for some time elementVerticalPosition
+          // highlights are displayed and horizontal
+          whitenAllFields()
+        }
+      } else {
+        draggedShipCopy.style.transform = ''
+        if (shipVerticalPosition === true) {
+          shipVerticalPosition = false
+          // Without whitenAllFields for some time elementVerticalPosition
+          // highlights are displayed and horizontal
+          whitenAllFields()
+        }
+      }
+    }
+  }
+
+  // Color all appropriate fields after placing on valid spot
+  const placeShip = (ev) => {
+    ev.preventDefault()
+    if (checkWrap(ev.target) || checkBlocked(ev.target)) {
+      return
+    }
+
+    const fields = getFields(ev.target)
+    fields.forEach((field) => {
+      field.classList.add('placed')
+
+      const coords = parseInt(field.dataset.coords)
+      blockFieldsAround(coords)
+
+      // Remove Listeners so that here no Element can be dropped anymore
+      field.removeEventListener('dragover', highlightFields)
+      field.removeEventListener('drop', placeShip)
+    })
+
+    // Remove original element and draggedElementCopy
+    const data = ev.dataTransfer.getData('text')
+    document.getElementById(data).remove()
+    draggedShipCopy.remove()
+  }
+
+  // Block all fields around one specified field
+  const blockFieldsAround = (coordsEl) => {
+    let blockedCoords = []
+
+    // Block fields on bottom side
+    blockedCoords.push(`${coordsEl + 9}`)
+    blockedCoords.push(`${coordsEl + 10}`)
+    blockedCoords.push(`${coordsEl + 11}`)
+
+    // Block fields on top side
+    blockedCoords.push(`${coordsEl - 9}`)
+    blockedCoords.push(`${coordsEl - 10}`)
+    blockedCoords.push(`${coordsEl - 11}`)
+
+    // Block fields on left and right side
+    blockedCoords.push(`${coordsEl - 1}`)
+    blockedCoords.push(`${coordsEl + 1}`)
+
+    blockedCoords.forEach((coords) => {
+      const el = document.querySelector(
+        `[data-coords='${coords < 10 ? 0 : ''}${coords}']`
+      )
+      if (el !== null) {
+        el.classList.add('blocked')
+      }
+    })
+  }
+
+  const createDragShips = (name, amount, sections) => {
+    for (let i = 0; i < amount; i++) {
       const dragShip = document.createElement('div')
+      dragShip.id = uniqid__WEBPACK_IMPORTED_MODULE_1___default()()
+      dragShip.dataset.sections = sections
       dragShip.classList.add(`ship-${name}`)
+      makeShipDraggable(dragShip)
       dragContainer.appendChild(dragShip)
     }
   }
 
   const createDragZone = () => {
     dragContainer.id = 'drag-container'
-    //1
-    createDragShips('big', 1) 
-    createDragShips('large', 2) 
-    createDragShips('medium', 3) 
-    createDragShips('small', 2) 
-    // const dragShipBig = document.createElement('div')
-    // dragShipBig.classList.add('ship-big')
-    // dragContainer.appendChild(dragShipBig)
-    // //2
-    // const dragShipLarge = document.createElement('div')
-    // dragShipLarge.classList.add('ship-large')
-    // dragContainer.appendChild(dragShipLarge)
-    // //3
-    // const dragShipMedium = document.createElement('div')
-    // dragShipMedium.classList.add('ship-medium')
-    // dragContainer.appendChild(dragShipMedium)
-    // //2
-    // const dragShipSmall = document.createElement('div')
-    // dragShipSmall.classList.add('ship-small')
-    // dragContainer.appendChild(dragShipSmall)
 
-    
-    
-    
-    
+    createDragShips('big', 1, 5)
+    createDragShips('large', 2, 4)
+    createDragShips('medium', 3, 3)
+    createDragShips('small', 2, 2)
 
     return dragContainer
   }
 
   const createBoards = (boardSize) => {
+    board1Container.classList.add('board-container')
+    board2Container.classList.add('board-container')
+
     board1Container.appendChild(playerName)
     board2Container.appendChild(computerName)
 
@@ -546,10 +767,10 @@ __webpack_require__.r(__webpack_exports__);
     boardsContainer.appendChild(board1Container)
     boardsContainer.appendChild(board2Container)
 
-    playerName.id = 'player-name'
-    computerName.id = 'computer-name'
+    playerName.classList.add('name')
+    computerName.classList.add('name')
 
-    boardsContainer.id = 'board-container'
+    boardsContainer.id = 'boards-container'
     board1.id = 'board-1'
     board2.id = 'board-2'
 
@@ -578,7 +799,7 @@ __webpack_require__.r(__webpack_exports__);
       finishMessage.style.display = 'none'
       resetBoard(board1)
       resetBoard(board2)
-      addFieldListeners(board2)
+      addClickListeners(board2)
     }
   }
 
@@ -604,11 +825,15 @@ __webpack_require__.r(__webpack_exports__);
     container.appendChild(finishMessage)
     container.appendChild(createBoards(boardSize))
     container.appendChild(createForm())
-    
+
     body.appendChild(heading)
     body.appendChild(container)
 
+    addDragDropListeners(board1)
+
     heading.innerHTML = 'Careship'
+
+    board2Container.style.display = 'none'
 
     finishMessage.id = 'finish-message'
     finishMessage.style.display = 'none'
